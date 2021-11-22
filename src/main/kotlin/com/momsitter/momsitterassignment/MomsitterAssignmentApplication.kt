a33100feafd60f105e3c5bd8b47f440dd84dbd43
rok93 <goodboy302@naver.com>
2021-11-22 15:25:19 +0900

    Initial commit

---
+++ src/main/kotlin/com/momsitter/momsitterassignment/MomsitterAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.momsitter.momsitterassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MomsitterAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<MomsitterAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/momsitter/momsitterassignment/MomsitterAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.momsitter.momsitterassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MomsitterAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
